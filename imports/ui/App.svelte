75bec0a9a6c06c1eebab294a6da237d1276348bf
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-01-20 13:09:34 +0100

    first commit

---
+++ imports/ui/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Router } from "@roxi/routify";
+  import { routes } from "../routify/routes.js";
+//   import { hello } from "../foo";
+</script>
+
+<Router {routes} />
+<!-- {hello} -->
---
+++ imports/ui/pages/index.svelte
@@ -0,0 +1,1 @@
+<h1>index svelte</h1>
